77378be66287f11c05ea19d8dda329386cfaac9c
Paulo Valarezo <paulinho.ec.1@gmail.com>
2021-12-18 19:24:29 -0500

    Mi nueva rama

---
+++ app/src/main/java/com/pst2021/pstta7g4/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.pst2021.pstta7g4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
